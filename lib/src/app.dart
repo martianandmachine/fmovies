36a08b5af876da504981fedb9c668c414e926783
Nikola <nikola@martian.agency>
2019-09-19 10:11:33 +0200

    Add new folder structure

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fmovies/src/presentation/popular/popular_movies_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PopularMoviesPage(title: 'Popular movies'),
+    );
+  }
+}
