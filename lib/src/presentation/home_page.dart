ed8358ddd131f75eb726795ebd4ce15a5ed1c563
Nikola <nikola@martian.agency>
2019-09-19 12:16:12 +0200

    Implement bottom navigation bar

--- lib/src/presentation/home_page.dart
+++ lib/src/presentation/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fmovies/src/presentation/popular/popular_movies_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +7,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final widgetOptions = [
+    PopularMoviesPage(),
+    Text('Favorites'),
+    Text('Cinemas'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-
+        child: widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            title: Text('Popular movies'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            title: Text('Cinemas'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
