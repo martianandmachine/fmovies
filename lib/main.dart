85935e7a91ea3eb08cb4d080e369060d26f741a5
Nikola <nikola@martian.agency>
2019-09-20 15:16:08 +0200

    Provide popular movies repository (get it)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fmovies/src/core/app.dart';
+import 'package:fmovies/src/features/popular/data/popular_movies_repository.dart';
+import 'package:fmovies/src/features/popular/data/popular_movies_repository_impl.dart';
+import 'package:get_it/get_it.dart';
 
-void main() => runApp(App());
+GetIt getIt = GetIt.instance;
+
+void main() {
+  getIt.registerFactory<PopularMoviesRepository>(
+    () => PopularMoviesRepositoryImpl(),
+  );
+
+  runApp(App());
+}
--- lib/src/features/popular/domain/popular_movies_bloc.dart
+++ lib/src/features/popular/domain/popular_movies_bloc.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:fmovies/src/core/utils/result.dart';
+import 'package:fmovies/src/features/popular/data/popular_movies_repository.dart';
 import 'package:fmovies/src/features/popular/domain/popular_movies_event.dart';
 import 'package:fmovies/src/features/popular/domain/popular_movies_state.dart';
+import 'package:get_it/get_it.dart';
 
 class PopularMoviesBloc extends Bloc<PopularMoviesEvent, PopularMoviesState> {
   @override
@@ -10,6 +13,11 @@ class PopularMoviesBloc extends Bloc<PopularMoviesEvent, PopularMoviesState> {
   Stream<PopularMoviesState> mapEventToState(PopularMoviesEvent event) async* {
     if (event is FetchPopularMovies) {
       print('Fetch movies triggered');
+
+      var popularMoviesRepository = GetIt.instance.get<PopularMoviesRepository>();
+
+      final Result results = await popularMoviesRepository.getPopularMovies();
+      
       yield PopularMoviesLoaded();
     }
   }
