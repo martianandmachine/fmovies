abab4fab2fbfcc65a85786927a554eb9c3d93d42
Nikola <nikola@martian.agency>
2019-09-20 11:39:00 +0200

    Add popular movies repository

--- lib/src/features/popular/data/popular_movies_repository.dart
+++ lib/src/features/popular/data/popular_movies_repository.dart
@@ -1,3 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fmovies/src/core/errors/exceptions.dart';
+import 'package:fmovies/src/features/popular/data/models/popular_movies_response.dart';
+
 abstract class PopularMoviesRepository {
 
+  Future<Either<PopularMoviesResponse, Exception>> getPopularMovies();
 }
--- lib/src/features/popular/data/popular_movies_repository_impl.dart
+++ lib/src/features/popular/data/popular_movies_repository_impl.dart
@@ -1,5 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:fmovies/src/core/errors/exceptions.dart';
+import 'package:fmovies/src/core/utils/network_info.dart';
+import 'package:fmovies/src/features/popular/data/models/popular_movies_response.dart';
 import 'package:fmovies/src/features/popular/data/popular_movies_repository.dart';
 
 class PopularMoviesRepositoryImpl implements PopularMoviesRepository {
+  @override
+  Future<Either<PopularMoviesResponse, Exception>> getPopularMovies() async {
+    
+    bool isConnected = await NetworkInfo().isConnected();
+    
+    if (isConnected) {
+      
+    } else {
+      Right(NoInternetException());
+    }
 
+  }
 }
