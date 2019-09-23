10bcf12c41f78bce5c309eec6ca0217aa4116f4e
Nikola <nikola@martian.agency>
2019-09-23 13:53:15 +0200

    Add bloc listener, add snackbar to show error

--- lib/src/features/popular/presentation/popular_movies_page.dart
+++ lib/src/features/popular/presentation/popular_movies_page.dart
@@ -9,32 +9,51 @@ class PopularMoviesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<PopularMoviesBloc>(context);
 
+    bloc.dispatch(FetchPopularMovies());
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Popular movies"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
-              builder: (context, state) {
-                if (state is PopularMoviesLoading) {
-                  return CircularProgressIndicator();
-                }
-                if (state is PopularMoviesLoaded) {
-                  return Text('Finished loading');
-                }
-                return Text('Something went wrong');
-              },
-            ),
-          ],
+      body: BlocListener<PopularMoviesBloc, PopularMoviesState>(
+        listener: (context, state) {
+          if (state is PopularMoviesNoInternet) {
+            _showSnackBar(context, 'Check yout internet connection.');
+          }
+          if (state is PopularMoviesServerError) {
+            _showSnackBar(context, 'Something went wrong with the server.');
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
+                builder: (context, state) {
+                  if (state is PopularMoviesLoading) {
+                    return CircularProgressIndicator();
+                  }
+                  if (state is PopularMoviesLoaded) {
+                    return Text('Finished loading');
+                  }
+                  return RaisedButton.icon(
+                    onPressed: () => bloc.dispatch(FetchPopularMovies()),
+                    icon: Icon(Icons.refresh),
+                    label: Text('Try again.'),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => bloc.dispatch(FetchPopularMovies()),
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    );
+  }
+
+  _showSnackBar(BuildContext context, String message) {
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
       ),
     );
   }
