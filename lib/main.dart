aac6412d185b3994a95cc15d24a49ac388806e67
Josip Zitkovic <josip@martian.agency>
2019-09-24 14:42:19 +0200

    fix cinemas repository injection

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:fmovies/src/core/api/movies_api_service.dart';
 import 'package:fmovies/src/core/api/movies_api_service_factory.dart';
 import 'package:fmovies/src/core/app.dart';
 import 'package:fmovies/src/core/utils/network_info.dart';
+import 'package:fmovies/src/features/cinemas/data/cinemas_repository.dart';
+import 'package:fmovies/src/features/cinemas/data/cinemas_repository_impl.dart';
 import 'package:fmovies/src/features/popular/data/popular_movies_repository.dart';
 import 'package:fmovies/src/features/popular/data/popular_movies_repository_impl.dart';
 import 'package:get_it/get_it.dart';
@@ -16,6 +18,11 @@ void main() {
 
   getIt.registerFactory<PopularMoviesRepository>(
     () => PopularMoviesRepositoryImpl(),
+  );
+
+
+  getIt.registerFactory<CinemasRepository>(
+        () => CinemasRepositoryImpl(),
   );
 
   runApp(App());
