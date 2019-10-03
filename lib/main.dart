f67cddea8abecb2ce560adc0b9e7da53d5eaa213
Nikola <nikola@martian.agency>
2019-10-03 13:36:52 +0200

    Move di to separate folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fmovies/src/core/app.dart';
-import 'package:fmovies/src/core/di.dart' as di;
+import 'package:fmovies/src/core/di/di.dart' as di;
 
 void main() async {
   await di.init();
--- lib/src/features/home_page.dart
+++ lib/src/features/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fmovies/src/core/di.dart';
+import 'package:fmovies/src/core/di/di.dart';
 import 'package:fmovies/src/features/favorites/domain/favorite_movies_bloc.dart';
 import 'package:fmovies/src/features/favorites/presentation/favorite_movies_page.dart';
 import 'package:fmovies/src/features/popular/domain/popular_movies_bloc.dart';
