36977dbddc6d6537e3bf78786f1daae2193744e4
Nikola <nikola@martian.agency>
2019-10-10 14:02:14 +0200

    Add app tab helper (for icon and title)

--- lib/src/features/home/domain/app_tab.dart
+++ lib/src/features/home/domain/app_tab.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+
 enum AppTab { popular, favorites, cinemas }
+
+class AppTabHelper {
+  static Icon getIcon(AppTab appTab) {
+    switch (appTab) {
+      case AppTab.popular:
+        return Icon(Icons.movie);
+      case AppTab.favorites:
+        return Icon(Icons.favorite);
+      case AppTab.cinemas:
+        return Icon(Icons.place);
+    }
+  }
+
+  static String getTitle(AppTab appTab) {
+    switch (appTab) {
+      case AppTab.popular:
+        return 'Popular';
+      case AppTab.favorites:
+        return 'Favorites';
+      case AppTab.cinemas:
+        return 'Cinemas';
+    }
+  }
+}
