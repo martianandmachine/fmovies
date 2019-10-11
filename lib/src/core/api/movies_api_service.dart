d41097bad2a04a6f1a311e49055db89c55a3c0d5
Josip Zitkovic <josip@martian.agency>
2019-10-11 11:24:26 +0200

    fetch credits

--- lib/src/core/api/movies_api_service.dart
+++ lib/src/core/api/movies_api_service.dart
@@ -6,4 +6,6 @@ abstract class MoviesApiService {
   Future<Response> getPopularMovies(int page);
 
   Future<Response> getMovieDetails(int movieId);
+
+  Future<Response> getMovieCredits(int movieId);
 }
