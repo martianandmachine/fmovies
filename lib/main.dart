5c61933f3bd4169eabfd955b33e21d83ff06d9b6
Nikola <nikola@martian.agency>
2019-10-18 15:47:12 +0200

    Add comment in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:fmovies/src/core/app.dart';
 import 'package:fmovies/src/core/di/di.dart' as di;
 
 void main() async {
+  // We need to access to the binary messenger before `runApp()` has been
+  // called (to read `secrets.json`)
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(App());
