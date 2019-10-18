1044f42989850f0f0ec24c1bd109571913eda89d
Nikola <nikola@martian.agency>
2019-10-18 13:26:42 +0200

    Remove double quotes

--- lib/src/core/api/cinemas_api_service_factoy.dart
+++ lib/src/core/api/cinemas_api_service_factoy.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:fmovies/src/core/api/cinemas_api_service.dart';
 
 class CinemasApiServiceFactory implements CinemasApiService {
-
   final Dio cinemasClient;
 
   CinemasApiServiceFactory({this.cinemasClient});
@@ -10,14 +9,14 @@ class CinemasApiServiceFactory implements CinemasApiService {
   @override
   Future<Response> getNearbyCinemas(double latitude, double longitude) {
     return cinemasClient.request(
-      "/maps/api/place/nearbysearch/json",
+      '/maps/api/place/nearbysearch/json',
       queryParameters: {
-        "location": "$latitude,$longitude",
-        "radius": "10000",
-        "type": "movie_theater",
+        'location': '$latitude,$longitude',
+        'radius': '10000',
+        'type': 'movie_theater',
       },
       options: Options(
-        method: "GET",
+        method: 'GET',
         responseType: ResponseType.plain,
       ),
     );
--- lib/src/core/api/movies_api_service_factory.dart
+++ lib/src/core/api/movies_api_service_factory.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:fmovies/src/core/api/movies_api_service.dart';
 
 class MoviesApiServiceFactory implements MoviesApiService {
-
   final Dio moviesClient;
 
   MoviesApiServiceFactory({this.moviesClient});
@@ -10,13 +9,13 @@ class MoviesApiServiceFactory implements MoviesApiService {
   @override
   getPopularMovies(int page) {
     return moviesClient.request(
-      "/3/movie/now_playing",
+      '/3/movie/now_playing',
       queryParameters: {
-        "sort_by": "popularity.desc",
-        "page": "$page",
+        'sort_by': 'popularity.desc',
+        'page': '$page',
       },
       options: Options(
-        method: "GET",
+        method: 'GET',
         responseType: ResponseType.plain,
       ),
     );
@@ -25,12 +24,12 @@ class MoviesApiServiceFactory implements MoviesApiService {
   @override
   getMovieDetails(int movieId) {
     return moviesClient.request(
-      "/3/movie/$movieId",
+      '/3/movie/$movieId',
       queryParameters: {
-        "append_to_response": "credits,videos,images",
+        'append_to_response': 'credits,videos,images',
       },
       options: Options(
-        method: "GET",
+        method: 'GET',
         responseType: ResponseType.plain,
       ),
     );
@@ -39,9 +38,9 @@ class MoviesApiServiceFactory implements MoviesApiService {
   @override
   getMovieCredits(int movieId) {
     return moviesClient.request(
-      "/3/movie/$movieId/credits",
+      '/3/movie/$movieId/credits',
       options: Options(
-        method: "GET",
+        method: 'GET',
         responseType: ResponseType.plain,
       ),
     );
